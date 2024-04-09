780bc793352d5ce8a290ea0f6f947c832f57bcfb
Salvi Jansen <salvi.jansen@gmail.com>
2024-04-09 09:26:59 +0200

    Added the hono router

--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 import { Ai } from '@cloudflare/ai';
+import { Hono } from 'hono';
+const app = new Hono();
 
-export default {
-	async fetch(_request, env, _ctx) {
-		const ai = new Ai(env.AI);
+app.get('/', async (c) => {
+	const ai = new Ai(c.env.AI);
 
-		const answer = await ai.run('@cf/meta/llama-2-7b-chat-int8', {
-			messages: [{ role: 'user', content: `What is the square root of 9?` }],
-		});
+	const answer = await ai.run('@cf/meta/llama-2-7b-chat-int8', {
+		messages: [{ role: 'user', content: `What is the square root of 9?` }],
+	});
 
-		return new Response(JSON.stringify(answer));
-	},
-};
+	return c.json(answer);
+});
+
+export default app;
