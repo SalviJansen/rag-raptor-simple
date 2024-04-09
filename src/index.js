27fc1844a3389840fec366943533a57fe6ceedcc
Salvi Jansen <salvi.jansen@gmail.com>
2024-04-09 09:12:49 +0200

    Activate AI binding and example generation

--- src/index.js
+++ src/index.js
@@ -8,8 +8,16 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { Ai } from '@cloudflare/ai';
+
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+	async fetch(_request, env, _ctx) {
+		const ai = new Ai(env.AI);
+
+		const answer = await ai.run('@cf/meta/llama-2-7b-chat-int8', {
+			messages: [{ role: 'user', content: `What is the square root of 9?` }],
+		});
+
+		return new Response(JSON.stringify(answer));
 	},
 };
